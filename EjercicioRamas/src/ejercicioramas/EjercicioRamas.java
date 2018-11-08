ff1f74c4fe13e212a4ef41cbe94454a440b22a59
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-11-08 09:11:46 +0100

    Añado un mensaje nuevo

--- EjercicioRamas/src/ejercicioramas/EjercicioRamas.java
+++ EjercicioRamas/src/ejercicioramas/EjercicioRamas.java
@@ -12,6 +12,7 @@ public class EjercicioRamas {
     public static void main(String[] args) {
        System.out.println("UN SALUDO A DAM1"); //Ln realiza un salto de linea
         System.out.println("Este es un ejemplo de ramas");
+        System.out.println("Me cambio a la master");
     }
 
 }
