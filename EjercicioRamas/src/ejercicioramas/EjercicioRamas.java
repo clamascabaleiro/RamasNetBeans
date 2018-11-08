b00e6cfb8edeb67a77a2f6ee5d93a28e4c9a46b3
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-11-08 09:04:29 +0100

    Añado un comentario

---
+++ EjercicioRamas/src/ejercicioramas/EjercicioRamas.java
@@ -0,0 +1,16 @@
+
+
+package ejercicioramas;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class EjercicioRamas {
+
+ 
+    public static void main(String[] args) {
+       System.out.println("UN SALUDO A DAM1"); //Ln realiza un salto de linea
+    }
+
+}
